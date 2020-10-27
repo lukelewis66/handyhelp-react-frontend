137956573ee370a69377d619fa17a864460a2c65
henryanderson64 <hankanderson135790@gmail.com>
2020-10-27 02:57:28 +0000

    ye

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
-import { Button, Modal } from "react-bootstrap";
+import { Form, Button, Modal, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+
 
 
 
@@ -18,9 +19,32 @@ const SignUp = () => {
 
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                    <Modal.Title>SignUp Component</Modal.Title>
+                    <Modal.Title>Sign Up Here</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>SignUp Form</Modal.Body>
+                <Modal.Body>
+                <Form>
+                    
+                    <Form.Group controlId="formBasicName">
+                        <Form.Label>First and Last Name</Form.Label>
+                        <Form.Control type="name" placeholder="Enter Name" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email Address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter Email" />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="I Agree to the Term and Conditions" />
+                    </Form.Group>
+                </Form>
+                </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
                         Close
