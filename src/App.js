b9aa61dff2820e84da1863dadb50d00f1e4bb326
Luke Lewis <lukelewis@ucsb.edu>
2020-11-11 02:38:32 +0000

    ll - moved getUserRole and call it after checkUserExists

--- src/App.js
+++ src/App.js
@@ -16,13 +16,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import IndividualListing from './components/pages/IndividualListing';
 import IndividualContractor from './components/pages/IndividualContractor.js';
 
-import { checkUserExists } from "./firebase/accountFunctions";
+import { checkUserExists, getUserRole } from "./firebase/accountFunctions";
 
 import { signOut } from "./firebase/authFunctions";
 
 //https://stackoverflow.com/questions/90178/make-a-div-fill-the-height-of-the-remaining-screen-space
 
 function App() {
+    const [isClient, setClient] = useState(2);
     const [accountSetup, setAccountSetup] = useState();
     const UID = localStorage.getItem("UID");
     const [userExists, setUserExists] = useState(true);
@@ -30,19 +31,19 @@ function App() {
     console.log("pageOnLoad: ", pageOnLoad);
 
     //signOut();
-    const renderAccountSetup = () => {
-        const UID = localStorage.getItem("UID");
-        if (UID) {
-            checkUserExists(UID)
-                .then(data => {
-                    if (data.exists === false) {
-                        setAccountSetup(<AccountSetup UID={UID} />)
-                    }
-                    console.log(data)
-                })
-                .catch(err => console.log(err));
-        }
-    }
+    // const renderAccountSetup = () => {
+    //     const UID = localStorage.getItem("UID");
+    //     if (UID) {
+    //         checkUserExists(UID)
+    //             .then(data => {
+    //                 if (data.exists === false) {
+    //                     setAccountSetup(<AccountSetup UID={UID} />)
+    //                 }
+    //                 console.log(data)
+    //             })
+    //             .catch(err => console.log(err));
+    //     }
+    // }
     useEffect(() => {
         //renderAccountSetup();
         console.log("UID: ", UID);
@@ -52,54 +53,74 @@ function App() {
                     if (data.exists === false) {
                         console.log("yo");
                         setUserExists(false);
+                    } else {
+                        getUserRole(UID)
+                            .then((response) => {
+                                if (response.role === "client") {
+                                    console.log("isClient = 1");
+                                    setClient(1);
+                                }
+                                else if (response.role === "contractor") {
+                                    console.log("isClient = 0");
+                                    setClient(0);
+                                }
+                                else if (response.role === "chill") {
+                                    console.log("User is chill");
+                                    setClient(2);
+                                }
+                                else {
+                                    console.log("isClient = 3");
+                                    setClient(3);
+                                }
+                            })
                     }
                 })
                 .catch(err => console.log(err));
         }
     }, [])
 
-    const [isClient, setClient] = useState(2);
-    
+
+
     useEffect(() => {
         const UID = localStorage.getItem("UID");
         var url;
         const server = process.env.REACT_APP_SERVER_URL;
-        url = new URL(`${server}/getrole`);
+        var url = new URL(`${server}/getrole`);
         const params = new URLSearchParams();
         params.append("UID", UID);
         url.search = params.toString();
         console.log(url.toString());
-            fetch(url)
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data.role);
-                    if(data.role === "client"){
-                      console.log("isClient = 1");
-                      setClient(1);
-                    }
-                    else if(data.role === "contractor"){
-                      console.log("isClient = 0");
-                      setClient(0);
-                    }
-                    else if(data.role === "chill"){
-                        console.log("User is chill");
-                        setClient(2);
-                    }
-                    else{
-                      console.log("isClient = 3");
-                      setClient(3);
-                    }
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.role);
+                if (data.role === "client") {
+                    console.log("isClient = 1");
+                    setClient(1);
                 }
+                else if (data.role === "contractor") {
+                    console.log("isClient = 0");
+                    setClient(0);
+                }
+                else if (data.role === "chill") {
+                    console.log("User is chill");
+                    setClient(2);
+                }
+                else {
+                    console.log("isClient = 3");
+                    setClient(3);
+                }
+            }
             )
             .catch((err) => console.log("rejected in App.js on: line 76"));
     }, [])
-  
-    
+
+
     return (
         <main>
             <div className="box">
                 <div className="box-header">
-                    <Navbar activepage={pageOnLoad} isClient = {isClient}/>
+                    <Navbar activepage={pageOnLoad} isClient={isClient} />
                 </div>
                 {/* box-body div will stretch to fill out the screen until footer (if its smaller than the screen) */}
                 <div className="box-body">
