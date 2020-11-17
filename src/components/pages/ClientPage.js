ba99bbe7f0c9d9cf39ebdccf4a5da27280022acc
henryanderson64 <hankanderson135790@gmail.com>
2020-11-17 04:01:57 +0000

    adding restrictions on pages and distance

--- src/components/pages/ClientPage.js
+++ src/components/pages/ClientPage.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
+import { getUserRole } from "../../firebase/accountFunctions";
+import { useEffect, useState } from "react";
 
 import ClientProfile from "../client/ClientProfile";
 
 const ClientPage = () => {
-    return (
-        <div>
-            <ClientProfile />
-        </div>
-    );
-}
+  return (
+    <div>
+      <ClientProfile />
+    </div>
+  );
+};
 
 export default ClientPage;
--- src/components/pages/IndividualListing.js
+++ src/components/pages/IndividualListing.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { initMap } from "../../gmaps/initMap.js"
 import { getUserInfo, getListing } from "../../firebase/Client";
+import { getUserLocation } from "../../firebase/accountFunctions.js";
+import { distance } from "../../gmaps/distance.js";
 
 
 const IndividualListing = () => {
@@ -12,7 +14,12 @@ const IndividualListing = () => {
         skilltags: [],
         title: "Name",
     });
-    const [userInfo, setUserinfo] = useState({});
+    const [thisUser, setThisUser] = useState({
+        location: [0,0]
+    });
+    const [userInfo, setUserinfo] = useState({
+        location: [0,0]
+    });
     let { LID } = useParams();
     LID = LID.substring(4, LID.length);
     useEffect(() => {
@@ -36,6 +43,11 @@ const IndividualListing = () => {
                 }
                 )
                 .catch((err) => console.log("rejected in IndividLisiting" + err));
+                var UID = localStorage.getItem("UID");
+                getUserLocation(UID).then((user) => {
+                    setApiCalls(apiCalls + 1);
+                    setThisUser(user);
+                })
         }
     }, [])
 
@@ -45,7 +57,9 @@ const IndividualListing = () => {
             <h1>Project: {listing.title}</h1>
             <h2>Description: {listing.description}</h2>
             <h2>client UID: {listing.client}</h2>
-            {/* <h2>data: {userInfo.location}</h2> still buggy*/}
+            <h2>location: {userInfo.location}</h2>
+            <h2>thisUser location: {thisUser.location}</h2>
+            <h2>Distance: {distance(thisUser.location[0],thisUser.location[1],userInfo.location[0],userInfo.location[1]).toFixed(1)} miles</h2>
             <h2>Api calls: {apiCalls}</h2>
         </div>
     );
