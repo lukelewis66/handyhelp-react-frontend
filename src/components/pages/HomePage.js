354fa03e43dd719d48de2e4bd62ce4c4edfa5fa6
Luke Lewis <lukelewis@ucsb.edu>
2020-12-03 02:31:03 +0000

    ll - start work on homepage. added some images

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,9 +1,56 @@
 import React, { useState } from "react";
+import { Carousel } from "react-bootstrap";
 
 const HomePage = () => {
     return (
         <div>
-            <h1>Welcome To HandyHelp!</h1>
+            <Carousel style={{ height: "700px", overflow: "hidden" }}>
+                <Carousel.Item className="homepage-carousel-item">
+                    <img
+                        className="d-block w-100 justify-content-center"
+                        src="/homepage-photos/pic1.jpg"
+                        alt="Image 1"
+                    />
+                </Carousel.Item>
+                <Carousel.Item className="homepage-carousel-item">
+                    <img
+                        className="d-block w-100 justify-content-center"
+                        src="/homepage-photos/pic2.jpg"
+                        alt="Image 2"
+                    />
+                </Carousel.Item>
+                <Carousel.Item className="homepage-carousel-item">
+                    <img
+                        className="d-block w-100 justify-content-center homepage-img-transform"
+                        src="/homepage-photos/pic3.jpg"
+                        alt="Image 3"
+                    />
+                </Carousel.Item>
+                <Carousel.Item className="homepage-carousel-item">
+                    <img
+                        className="d-block w-100 justify-content-center homepage-img-transform"
+                        src="/homepage-photos/pic4.jpg"
+                        alt="Image 4"
+                    />
+                </Carousel.Item>
+                <Carousel.Item className="homepage-carousel-item">
+                    <img
+                        className="d-block w-100 justify-content-center"
+                        src="/homepage-photos/pic5.jpg"
+                        alt="Image 5"
+                    />
+                </Carousel.Item>
+                <Carousel.Item className="homepage-carousel-item">
+                    <img
+                        className="d-block w-100 justify-content-center"
+                        src="/homepage-photos/pic6.jpg"
+                        alt="Image 6"
+                    />
+                </Carousel.Item>
+            </Carousel>
+            <div className="homepage-main-text">
+                <h1>HandyHelp</h1>
+            </div>
         </div>
     );
 }
